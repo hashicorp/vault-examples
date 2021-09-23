174aa4cb8cbe66c59b045dcb343818b0f7beca4f
VAL <val@hashicorp.com>
2021-09-23 15:56:09 -0700

    Add token renewal example in Go (#3)
    * Add token renewal example in Go
    
    * Add additional clarifying comments and nil checks

--- setup.sh
+++ setup.sh
@@ -12,6 +12,17 @@ curl -X PUT -H "X-Vault-Token: ${VAULT_DEV_ROOT_TOKEN_ID}" -d '{"policy":"path \
 echo "Creating secret"
 curl -X PUT -H "X-Vault-Token: ${VAULT_DEV_ROOT_TOKEN_ID}" -d '{"data": {"password": "Hashi123"}}' ${VAULT_ADDR}/v1/kv-v2/data/creds
 
+## USERPASS
+echo "Enabling userpass auth"
+curl -X POST -H "X-Vault-Token: ${VAULT_DEV_ROOT_TOKEN_ID}" -d '{"type": "userpass"}' ${VAULT_ADDR}/v1/sys/auth/userpass
+
+echo "Creating user with dev-policy for userpass auth method"
+curl -X PUT -H "X-Vault-Token: ${VAULT_DEV_ROOT_TOKEN_ID}" -d '{"password":"my-password", "policies":"dev-policy"}' ${VAULT_ADDR}/v1/auth/userpass/users/my-user
+
+echo "Set token TTLs for all tokens created by userpass mount"
+# This is a ridiculously small max TTL only to get the tests to end the LifetimeWatcher faster and not actually use the large Increment value proposed by the code sample.
+curl -X PUT -H "X-Vault-Token: ${VAULT_DEV_ROOT_TOKEN_ID}" -d '{"default_lease_ttl":"3s","max_lease_ttl":"5s"}' ${VAULT_ADDR}/v1/sys/auth/userpass/tune
+
 ## APPROLE
 echo "Enabling approle auth"
 curl -X POST -H "X-Vault-Token: ${VAULT_DEV_ROOT_TOKEN_ID}" -d '{"type": "approle"}' ${VAULT_ADDR}/v1/sys/auth/approle
