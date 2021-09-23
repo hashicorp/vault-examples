9bcfcddb22843385cc7b43755ecb267d929d9c23
Valerie Conklin <val@hashicorp.com>
2021-09-23 12:08:03 -0700

    Add token renewal example in Go

--- go/examples_test.go
+++ go/examples_test.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"log"
 	"os"
 	"testing"
+
+	vault "github.com/hashicorp/vault/api"
 )
 
 var expected = os.Getenv("EXPECTED_SECRET_VALUE")
@@ -14,6 +17,50 @@ func TestGetSecret(t *testing.T) {
 	}
 	if value != expected {
 		t.Fatalf("Expected %s, but got %s", expected, value)
+	}
+}
+
+func TestRenewToken(t *testing.T) {
+	config := vault.DefaultConfig()
+
+	client, err := vault.NewClient(config)
+	if err != nil {
+		t.Fatalf("Failed to initialize Vault client: %v", err)
+	}
+
+	vaultLoginResp, err := login(client)
+	if err != nil {
+		log.Fatalf("unable to authenticate to Vault: %v", err)
+	}
+
+	token, err := client.Auth().Token().LookupSelf()
+	if err != nil {
+		t.Fatalf("Failed to lookup token on client: %v", err)
+	}
+
+	originalTokenExpiration, exists := token.Data["expire_time"]
+	if !exists {
+		t.Fatalf("Token secret does not have expire_time.")
+	}
+
+	renewErr := manageTokenLifecycle(client, vaultLoginResp) // can't just pass our var called "token" because the api.Secret returned by LookupSelf() does not contain an Auth object
+	if renewErr != nil {
+		log.Fatalf("unable to start managing token lifecycle: %v", renewErr)
+	}
+
+	// we should have renewed at least one time
+	renewedToken, err := client.Auth().Token().LookupSelf()
+	if err != nil {
+		t.Fatalf("Failed to lookup renewed token on client: %v", err)
+	}
+
+	newTokenExpiration, exists := renewedToken.Data["expire_time"]
+	if !exists {
+		t.Fatalf("Renewed token secret does not have expire_time.")
+	}
+
+	if originalTokenExpiration == newTokenExpiration {
+		t.Fatalf("Token had same expiration time before and after calling for renewal. Renewal did not occur.")
 	}
 }
 
