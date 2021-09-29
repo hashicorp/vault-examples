9b3a86208be6517106a51b0d76a86f4f606a53be
Valerie Conklin <val@hashicorp.com>
2021-09-29 16:41:53 -0700

    Make explanatory comment more concise

--- go/6_auth-kubernetes.go
+++ go/6_auth-kubernetes.go
@@ -10,13 +10,14 @@ import (
 // Fetches a key-value secret (kv-v2) after authenticating to Vault with a Kubernetes service account.
 //
 // As the client, all we need to do is pass along the JWT token representing our application's Kubernetes Service Account in our login request to Vault.
-// This token is automatically mounted to your application's container by Kubernetes.
+// This token is automatically mounted to your application's container by Kubernetes. Read more at https://www.vaultproject.io/docs/auth/kubernetes
 //
-// SETUP NOTES: The Vault server must first also be configured to be able to communicate with the Kubernetes API, to
-// verify that the client's token is from the service-account it says it is. If an operator hasn't already done this for you, you will do so like this:
-//    // TOKEN_REVIEWER_SECRET is the name of the Kubernetes Secret representing the token for whichever service account will be used to communicate with Kubernetes' TokenReview API to approve tokens. That serviceaccount needs the ClusterRole system:auth-delegator.
+// SETUP NOTES: If an operator has not already set up Kubernetes auth in Vault for you, then you must also first configure the Vault server with its own Service Account token to be able to communicate with the Kubernetes API
+// so it can verify that the client's service-account token is valid. The service account that will be performing that verification needs the ClusterRole system:auth-delegator.
+//
 //    export TOKEN_REVIEW_JWT=$(kubectl get secret $TOKEN_REVIEWER_SECRET --output='go-template={{ .data.token }}' | base64 --decode)
 //    export KUBE_HOST=$(kubectl config view --raw --minify --flatten --output='jsonpath={.clusters[].cluster.server}')
+//    kubectl config view --raw --minify --flatten --output='jsonpath={.clusters[].cluster.certificate-authority-data}' | base64 --decode > path/to/kube_ca_cert
 //
 //    vault write auth/kubernetes/config \
 //  	token_reviewer_jwt=${TOKEN_REVIEW_JWT} \
@@ -26,7 +27,6 @@ import (
 //
 // The "issuer" field only needs to be set when running Kubernetes version 1.21 or above:
 // https://www.vaultproject.io/docs/platform/k8s/csi#setting-issuer-for-kubernetes-authentication.
-// Issuer validation can also be disabled with the flag disable_iss_validation=true
 //
 // Finally, make sure to create a role in Vault bound to your pod's service account:
 //
@@ -35,7 +35,7 @@ import (
 //     	bound_service_account_names="my-app" \
 //		bound_service_account_namespaces="default"
 func getSecretWithKubernetesAuth() (string, error) {
-	// If not specified, the VAULT_ADDR environment variable (if set) will be the address that your pod uses to communicate with Vault.
+	// If set, the VAULT_ADDR environment variable will be the address that your pod uses to communicate with Vault.
 	// For an application running in Kubernetes accessing a Vault server outside of Kubernetes, you will need to set VAULT_ADDR in your pod spec to
 	// point at your Kubernetes cluster's gateway address. See this tutorial for more information: https://learn.hashicorp.com/tutorials/vault/kubernetes-external-vault
 	config := vault.DefaultConfig() // modify for more granular configuration
