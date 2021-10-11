46075fe43a6359342a8f0fcaf5909517b58e594b
Annalise Diroff <adiroff@hashicorp.com>
2021-10-10 19:04:29 -0700

    Initial go example

---
+++ go/7_auth-azure.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	vault "github.com/hashicorp/vault/api"
+)
+
+func getSecretWithAzureAuth() (string, error) {
+	config := vault.DefaultConfig() // modify for more granular configuration
+
+	client, err := vault.NewClient(config)
+	if err != nil {
+		return "", fmt.Errorf("unable to initialize Vault client: %w", err)
+	}
+	
+	// azure client
+
+	// get secret
+	secret, err := client.Logical().Read("kv-v2/data/creds")
+	if err != nil {
+		return "", fmt.Errorf("unable to read secret: %w", err)
+	}
+
+	data, ok := secret.Data["data"].(map[string]interface{})
+	if !ok {
+		return "", fmt.Errorf("data type assertion failed: %T %#v", secret.Data["data"], secret.Data["data"])
+	}
+
+	// data map can contain more than one key-value pair, in this case we're just grabbing one of them
+	key := "password"
+	value, ok := data[key].(string)
+	if !ok {
+		return "", fmt.Errorf("value type assertion failed: %T %#v", data[key], data[key])
+	}
+
+	return value, nil
+}
