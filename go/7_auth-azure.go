6f4402e5688794cedef42b60cb73d8db1ad30c73
Annalise Diroff <adiroff@hashicorp.com>
2021-10-11 20:47:16 -0700

    Adding getting jwt

--- go/7_auth-azure.go
+++ go/7_auth-azure.go
@@ -2,8 +2,22 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"encoding/json"
 	vault "github.com/hashicorp/vault/api"
 )
+
+type responseJson struct {
+	AccessToken string `json:"access_token"`
+	RefreshToken string `json:"refresh_token"`
+	ExpiresIn string `json:"expires_in"`
+	ExpiresOn string `json:"expires_on"`
+	NotBefore string `json:"not_before"`
+	Resource string `json:"resource"`
+	TokenType string `json:"token_type"`
+  }
 
 func getSecretWithAzureAuth() (string, error) {
 	config := vault.DefaultConfig() // modify for more granular configuration
@@ -34,4 +48,51 @@ func getSecretWithAzureAuth() (string, error) {
 	}
 
 	return value, nil
+}
+
+func getJWT() (string, error) {
+	
+	// Create HTTP request for a managed services for Azure resources token to access Azure Resource Manager
+    var msi_endpoint *url.URL
+    msi_endpoint, err := url.Parse("http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01")
+    if err != nil {
+      fmt.Println("Error creating URL: ", err)
+      return "", err
+    }
+
+	msi_parameters := msi_endpoint.Query()
+    msi_parameters.Add("resource", "https://management.azure.com/")
+    msi_endpoint.RawQuery = msi_parameters.Encode()
+    req, err := http.NewRequest("GET", msi_endpoint.String(), nil)
+    if err != nil {
+      fmt.Println("Error creating HTTP request: ", err)
+      return "", err
+    }
+    req.Header.Add("Metadata", "true")
+
+    // Call managed services for Azure resources token endpoint
+    client := &http.Client{}
+    resp, err := client.Do(req) 
+    if err != nil{
+      fmt.Println("Error calling token endpoint: ", err)
+      return "", err
+    }
+
+    // Pull out response body
+    responseBytes,err := ioutil.ReadAll(resp.Body)
+    defer resp.Body.Close()
+    if err != nil {
+      fmt.Println("Error reading response body : ", err)
+      return "", err
+    }
+
+    // Unmarshall response body into struct
+	var r responseJson
+    err = json.Unmarshal(responseBytes, &r)
+    if err != nil {
+      fmt.Println("Error unmarshalling the response:", err)
+      return "", err
+    }
+
+	return r.AccessToken, nil
 }
