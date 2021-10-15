d91d5888138a739cc8dbc67f87e6c9c3b2a32e43
Annalise Diroff <adiroff@hashicorp.com>
2021-10-15 10:44:05 -0700

    Get metadata

--- go/7_auth-azure.go
+++ go/7_auth-azure.go
@@ -19,6 +19,12 @@ type responseJson struct {
 	TokenType string `json:"token_type"`
   }
 
+  type metadataJson struct {
+	  VirtualMachineName string `json:"name"`
+	  SubscriptionId string `json:"subsscriptionId"`
+	  ResourceGroupName string `json:"resourceGroupName"`
+  }
+
 func getSecretWithAzureAuth() (string, error) {
 	config := vault.DefaultConfig() // modify for more granular configuration
 
@@ -30,6 +36,22 @@ func getSecretWithAzureAuth() (string, error) {
 	// get JWT
 	jwtResp, err := getJWT()
 
+		// log in to Vault's GCP auth method with signed JWT token
+	params := map[string]interface{}{
+			"role": "dev-role-iam", // the name of the role in Vault that was created with this IAM bound to it
+			"jwt":  jwtResp,
+	}
+
+	// log in to Vault's Azure auth method
+	resp, err := client.Logical().Write("auth/azure/login", params) // confirm with your Vault administrator that "aws" is the correct mount name
+	if err != nil {
+		return "", fmt.Errorf("unable to log in with AWS IAM auth: %w", err)
+	}
+	if resp == nil || resp.Auth == nil || resp.Auth.ClientToken == "" {
+		return "", fmt.Errorf("login response did not return client token")
+	}
+
+	client.SetToken(resp.Auth.ClientToken)
 	// get secret
 	secret, err := client.Logical().Read("kv-v2/data/creds")
 	if err != nil {
@@ -49,6 +71,52 @@ func getSecretWithAzureAuth() (string, error) {
 	}
 
 	return value, nil
+}
+
+func getMetadata() (metadataJson, error) {
+
+	var msi_endpoint *url.URL
+	msi_endpoint, err := url.Parse("http://169.254.169.254/metadata/instance?api-version=2017-08-01")
+	if err != nil {
+  		fmt.Println("Error creating URL: ", err)
+  		return metadataJson{}, err
+	}
+
+	msi_parameters := msi_endpoint.Query()
+	msi_parameters.Add("resource", "https://management.azure.com/")
+	msi_endpoint.RawQuery = msi_parameters.Encode()
+	req, err := http.NewRequest("GET", msi_endpoint.String(), nil)
+	if err != nil {
+		fmt.Println("Error creating HTTP request: ", err)
+		return metadataJson{}, err
+	}
+	req.Header.Add("Metadata", "true")
+
+    // Call managed services for Azure resources token endpoint
+    client := &http.Client{}
+    resp, err := client.Do(req) 
+    if err != nil{
+      fmt.Println("Error calling token endpoint: ", err)
+      return metadataJson{}, err
+    }
+
+    // Pull out response body
+    responseBytes,err := ioutil.ReadAll(resp.Body)
+    defer resp.Body.Close()
+    if err != nil {
+      fmt.Println("Error reading response body : ", err)
+      return metadataJson{}, err
+    }
+
+    // Unmarshall response body into struct
+	var r responseJson
+    err = json.Unmarshal(responseBytes, &r)
+    if err != nil {
+      fmt.Println("Error unmarshalling the response:", err)
+      return metadataJson{}, err
+    }
+
+	return metadataJson{}, nil	
 }
 
 func getJWT() (string, error) {
