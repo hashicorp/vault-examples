9ead303b684086c03752a3028cbef0b51f5e1c6c
Valerie Conklin <val@hashicorp.com>
2022-02-04 12:30:46 -0800

    Add full version of quickstart code

---
+++ examples/_quick-start/dotnet/Example.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using VaultSharp;
+using VaultSharp.V1.AuthMethods;
+using VaultSharp.V1.AuthMethods.Token;
+using VaultSharp.V1.Commons;
+
+// This is the full version of the code accompanying the Developer Quick Start.
+// WARNING: Using root tokens is insecure and should never be done in production!
+namespace DeveloperQuickstart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Authentication
+            IAuthMethodInfo authMethod = new TokenAuthMethodInfo(vaultToken: "dev-only-token");
+
+            VaultClientSettings vaultClientSettings = new VaultClientSettings("http://127.0.0.1:8200", authMethod);
+            IVaultClient vaultClient = new VaultClient(vaultClientSettings);
+
+            // Writing a secret
+            var secretData = new Dictionary<string, object> { { "password", "Hashi123" } };
+            vaultClient.V1.Secrets.KeyValue.V2.WriteSecretAsync(
+                path: "/my-secret-password",
+                data: secretData,
+                mountPoint: "secret"
+            ).Wait();
+
+            Console.WriteLine("Secret written successfully.");
+
+            // Reading a secret
+            Secret<SecretData> secret = vaultClient.V1.Secrets.KeyValue.V2.ReadSecretAsync(
+                path: "/my-secret-password",
+                mountPoint: "secret"
+            ).Result;
+
+            var password = secret.Data.Data["password"];
+
+            if (password.ToString() != "Hashi123")
+            {
+                throw new System.Exception("Unexpected password");
+            }
+
+            Console.WriteLine("Access granted!");
+        }
+    }
+}
